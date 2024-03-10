5bb7fddc500c3ef927afb0ad83f59d4a3520a5d1
Yashwant2704 <155355507+Yashwant2704@users.noreply.github.com>
2024-03-10 15:33:15 +0530

    Add CAPTCHA

--- index.js
+++ index.js
@@ -139,6 +139,18 @@ function redirect_to_register() {
   window.location.href = "register.html";
 }
 
+// Logout function
+function logout() {
+  auth.signOut().then(function() {
+    // Sign-out successful.
+    window.location.href = "login.html";
+  }).catch(function(error) {
+    // An error happened.
+    console.error('Error logging out:', error.message);
+  });
+}
+
+
 // Password reset function
 function resetPassword() {
   var email = document.getElementById('email').value;
@@ -156,3 +168,63 @@ function resetPassword() {
       alert('Error resetting password: ' + error.message);
     });
 }
+
+
+//CAPTCHA SCRIPT:
+// Selecting necessary DOM elements
+const captchaTextBox = document.querySelector(".captch_box input");
+const refreshButton = document.querySelector(".refresh_button");
+const captchaInputBox = document.querySelector(".captch_input input");
+const message = document.querySelector(".message");
+const submitButton = document.querySelector(".button");
+
+// Variable to store generated captcha
+let captchaText = null;
+
+// Function to generate captcha
+const generateCaptcha = () => {
+  const randomString = Math.random().toString(36).substring(2, 7);
+  const randomStringArray = randomString.split("");
+  const changeString = randomStringArray.map((char) => (Math.random() > 0.5 ? char.toUpperCase() : char));
+  captchaText = changeString.join("   ");
+  captchaTextBox.value = captchaText;
+  console.log(captchaText);
+};
+
+const refreshBtnClick = () => {
+  generateCaptcha();
+  captchaInputBox.value = "";
+  captchaKeyUpValidate();
+};
+
+const captchaKeyUpValidate = () => {
+  //Toggle submit button disable class based on captcha input field.
+  submitButton.classList.toggle("disabled", !captchaInputBox.value);
+
+  if (!captchaInputBox.value) message.classList.remove("active");
+};
+
+// Function to validate the entered captcha
+const submitBtnClick = () => {
+  captchaText = captchaText
+    .split("")
+    .filter((char) => char !== " ")
+    .join("");
+  message.classList.add("active");
+  // Check if the entered captcha text is correct or not
+  if (captchaInputBox.value === captchaText) {
+    message.innerText = "Entered captcha is correct";
+    message.style.color = "#826afb";
+  } else {
+    message.innerText = "Entered captcha is not correct";
+    message.style.color = "#FF2525";
+  }
+};
+
+// Add event listeners for the refresh button, captchaInputBox, submit button
+refreshButton.addEventListener("click", refreshBtnClick);
+captchaInputBox.addEventListener("keyup", captchaKeyUpValidate);
+submitButton.addEventListener("click", submitBtnClick);
+
+// Generate a captcha when the page loads
+generateCaptcha();
